3c90a8d76fa577aa0d37d919ed6411db5a10491e
Prasanna Kumar <prasannaboga@gmail.com>
2023-11-21 11:25:24 +0530

    updated spec with simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,9 @@ require "simplecov"
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-SimpleCov.start "rails"
+SimpleCov.start do
+  add_filter "/spec/"
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
